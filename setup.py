022dfb5c84b4aa692d4ef09f2426d9c0e20932b2
Xuanwo <xuanwo.cn@gmail.com>
2016-03-19 16:52:00 +0800

    use docopt as argument parser and finish group choose & list & send func , need more test and some files should be removed

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     entry_points=entry_points,
-    requires=['requests', 'flask'],
+    requires=['requests', 'docopt', 'pyyaml'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
