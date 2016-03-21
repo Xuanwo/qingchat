994f0767d6efced59236afd8334ba5194d3df0de
Xuanwo <xuanwo.cn@gmail.com>
2016-03-21 15:17:26 +0800

    fix: fix commmit message error

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+# 0.0.3
+
+## Features
+
+- `group send` support send media like pics.
+- `group choose` support regex like `Qingchat_test_*`
+
+## Bug Fixes
+
+- Fix error while config['group'] is empty
+
+## Breaking Changes
+
+- Do not support `group choose` without option anymore
+
+# 0.0.2
+
+## Features
+
+- Support `group` command with `list`, `choose`, `send`
+
+## Bug Fixes
+
+- Fix config file in different platform
+
+# 0.0.1
+
+## Features
+
+- Support CLI
--- qingchat/cli.py
+++ qingchat/cli.py
@@ -19,49 +19,91 @@ import requests
 import yaml
 import os
 from docopt import docopt
+import re
 
 
 def init():
+    """
+
+    :return: the content fo config file
+    """
     global home
     home = os.getenv('HOME') + '/.config/qingchat'  # need to be tested on Mac OS, and do not support Win
+    initconfig = dict()
 
     if not os.path.exists(home):  # create dir for config file
         os.makedirs(home)
     if not os.path.isfile(home + '/config.yml'):  # create config file if noy exist
         os.mknod(home + '/config.yml')
-        with open(home + '/config.yml', "w") as f:
-            initconfig = dict()
-            initconfig['address'] = "127.0.0.1"
-            initconfig['port'] = 3000
-            f.write(yaml.dump(initconfig, default_flow_style=False))
-            f.close()
+        initconfig['address'] = "127.0.0.1"
+        initconfig['port'] = 3000
+        write_config(initconfig)
+    else:
+        initconfig = load_config()
+
+    return initconfig
+
+
+def write_config(content):
+    """
+
+    :param content: content of config
+    :return:
+    """
+    with open(home + '/config.yml', "w") as f:
+        f.write(yaml.dump(content, default_flow_style=False))
+        f.close()
+
+
+def load_config():
+    """
+
+    :return: yaml of config file
+    """
+    with open(home + '/config.yml', "r") as f:
+        content = yaml.load(f)
+        f.close()
+    return content
 
 
 def group_list():
+    """
+
+    :return: the json of group info
+    """
     url = address + 'get_group_info'  # get wechat group info
     r = requests.get(url)
     print("您的群组为：")
+    config['group'] = []
     content = r.json()
     for i in content:
-        print("群名称： " + i["displayname"])
+        config['group'].append(i['displayname'])
+        print("群名称： " + i['displayname'])
+    write_config(config)
+    return config['group']
 
 
 def group_choose(group_name):
     """
-    :param group_name: group_name to be chosen
-    :return:
+    :param group_name: group_name to be chosen, support re
+    :return: list of chonsen groups
     """
-    if 'chosen_group' not in config:
+    if 'chosen_group' not in config or not config['chosen_group']:
         config['chosen_group'] = []
+    if not config['chosen_group']:
+        config['chosen_group'] = []
+
     for i in group_name:
-        if i not in config['chosen_group']:
-            config['chosen_group'].append(i)
-    with open(home + '/config.yml', 'w') as f:
-        f.write(yaml.dump(config, default_flow_style=False))
-        f.close()
+        for j in config['group']:
+            if re.match(i, j) and j not in config['chosen_group']:
+                config['chosen_group'].append(j)
+
+    write_config(config)
     print("您已经选择的群组：")
     for i in config['chosen_group']:
         print("群名称： " + i)
+
+    return config['chosen_group']
 
 
 def group_send_text(content):
@@ -107,9 +149,7 @@ def group_clean():
     """
     if 'chosen_group' in config:
         del config['chosen_group']
-        with open(home + '/config.yml', 'w') as f:
-            f.write(yaml.dump(config, default_flow_style=False))
-            f.close()
+        write_config(config)
         print("您选中的群组均已被删除。")
 
 
@@ -118,12 +158,9 @@ def main():
 
     :return:
     """
-    arguments = docopt(__doc__, version='Qingchat 0.0.1')
-    init()
+    arguments = docopt(__doc__, version='Qingchat 0.0.2')
     global config, address
-    with open(home + '/config.yml', 'r') as f:  # load config file
-        config = yaml.load(f)
-        f.close()
+    config = init()
     address = 'http://%s:%d/openwx/' % (config['address'], config['port'])
 
     if arguments['group']:  # group command
@@ -137,7 +174,7 @@ def main():
             group_send_image(arguments['<media>'])
         elif arguments['clean']:
             group_clean()
-    elif arguments['user']:  # group command
+    elif arguments['user']:  # user command
         pass
 
 
