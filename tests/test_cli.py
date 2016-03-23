971a9d755c0f1687995402c601ce09391e8c5b57
Xuanwo <xuanwo.cn@gmail.com>
2016-03-23 20:44:51 +0800

    use another branch to develop

---
+++ tests/test_cli.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from qingchat import cli
+from unittest import mock
+import unittest
+
+
+class QingchatTestCase(unittest.TestCase):
+    """
+    Tests for cli.py
+    """
+
+    def test_init(self):
+        pass
+
+    def test_save_config(self):
+        pass
+
+    def test_load_config(self):
+        pass
+
+    def test_config_ip(self):
+        pass
+
+    def test_config_port(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_group_list(self):
+        pass
+
+    def test_group_choose(self):
+        pass
+
+    def test_group_send_text(self):
+        pass
+
+    def test_group_send_media(self):
+        pass
+
+    def test_group_send_by_file(self):
+        pass
+
+    def test_group_clean(self):
+        pass
+
+    def test_main(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
