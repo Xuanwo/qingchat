f9d1a16593d0c7185c8c4d9f2f837f78a8a2f10f
Xuanwo <xuanwo.cn@gmail.com>
2016-04-04 16:09:53 +0800

    add test for cli.init

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,6 +10,7 @@ import yaml
 
 def setup():
     os.environ['HOME'] = os.getcwd()  # set test env
+    global home
     home = os.getenv('HOME') + '/.config/qingchat'
     os.makedirs('.config/qingchat')
     initconfig = {
@@ -22,7 +23,6 @@ def setup():
 
 
 def teardown():
-    home = os.getenv('HOME') + '/.config/qingchat'
     if os.path.isfile(home + '/config.yml'):
         os.remove(home + '/config.yml')
     if os.path.exists(home):
@@ -30,8 +30,12 @@ def teardown():
 
 
 def test_init():
-    pass
-
+    teardown()
+    testconfig = {
+        'ip': '127.0.0.1',
+        'port': 3000
+    }
+    assert cli.init()==testconfig
 
 def test_save_config():
     pass
