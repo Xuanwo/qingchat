8de64a86942b0b41eac6506a370f1f6a204ec01f
Xuanwo <xuanwo.cn@gmail.com>
2016-04-04 16:00:29 +0800

    finished setup and teardown

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,54 +2,80 @@
 # -*- coding: utf-8 -*-
 
 from qingchat import cli
+import nose
+import os
 from unittest import mock
-import unittest
+import yaml
 
 
-class QingchatTestCase(unittest.TestCase):
-    """
-    Tests for cli.py
-    """
-
-    def test_init(self):
-        pass
-
-    def test_save_config(self):
-        pass
-
-    def test_load_config(self):
-        pass
-
-    def test_config_ip(self):
-        pass
-
-    def test_config_port(self):
-        pass
-
-    def test_login(self):
-        pass
-
-    def test_group_list(self):
-        pass
-
-    def test_group_choose(self):
-        pass
-
-    def test_group_send_text(self):
-        pass
-
-    def test_group_send_media(self):
-        pass
-
-    def test_group_send_by_file(self):
-        pass
-
-    def test_group_clean(self):
-        pass
-
-    def test_main(self):
-        pass
+def setup():
+    os.environ['HOME'] = os.getcwd()  # set test env
+    home = os.getenv('HOME') + '/.config/qingchat'
+    os.makedirs('.config/qingchat')
+    initconfig = {
+        'ip': '127.0.0.1',
+        'port': 3000
+    }
+    with open(home + '/config.yml', 'w+') as f:
+        f.write(yaml.dump(initconfig, default_flow_style=False))
+        f.close()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def teardown():
+    home = os.getenv('HOME') + '/.config/qingchat'
+    if os.path.isfile(home + '/config.yml'):
+        os.remove(home + '/config.yml')
+    if os.path.exists(home):
+        os.removedirs(home)
+
+
+def test_init():
+    pass
+
+
+def test_save_config():
+    pass
+
+
+def test_load_config():
+    pass
+
+
+def test_config_ip():
+    pass
+
+
+def test_config_port():
+    pass
+
+
+def test_login():
+    pass
+
+
+def test_group_list():
+    pass
+
+
+def test_group_choose():
+    pass
+
+
+def test_group_send_text():
+    pass
+
+
+def test_group_send_media():
+    pass
+
+
+def test_group_send_by_file():
+    pass
+
+
+def test_group_clean():
+    pass
+
+
+def test_main():
+    pass
